c3c14d953fbdb0eefd8fc65b29d942ed8d7b1781
jon-alamo <jonrivala@gmail.com>
2020-11-11 19:23:17 +0100

    User model with member related name

--- api/models/member.py
+++ api/models/member.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from api.models.membership import Membership
 from api.models.address import Address
@@ -6,6 +7,9 @@ from api.models.address import Address
 class Member(models.Model):
     number = models.IntegerField(primary_key=True)
     memberships = models.ManyToManyField(Membership)
+    user = models.OneToOneField(
+        to=User, on_delete=models.CASCADE, related_name='member'
+    )
     email = models.EmailField()
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
--- api/models/user.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User as AuthUser
-
-from api.models.member import Member
-
-
-class User(AuthUser):
-    member = models.OneToOneField(Member, on_delete=models.CASCADE)
-
