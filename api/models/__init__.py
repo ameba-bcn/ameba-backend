83afc6fca8fa688610d091c57cfb278a64876a77
jon-alamo <jonrivala@gmail.com>
2020-11-12 18:00:37 +0100

    new files and migrrations: not working, User.email not unique

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,7 @@
 from api.models.address import Address
 from api.models.member import Member
 from api.models.membership import Membership
+from django.contrib.auth.models import User
+
+User.USERNAME_FIELD = User.EMAIL_FIELD
+User.REQUIRED_FIELDS = []
--- api/models/member.py
+++ api/models/member.py
@@ -1,3 +1,4 @@
+from django.core import exceptions
 from django.contrib.auth.models import User
 from django.db import models
 from api.models.membership import Membership
@@ -6,13 +7,26 @@ from api.models.address import Address
 
 class Member(models.Model):
     number = models.IntegerField(primary_key=True)
-    memberships = models.ManyToManyField(Membership)
+    memberships = models.ManyToManyField(Membership, blank=True)
     user = models.OneToOneField(
-        to=User, on_delete=models.CASCADE, related_name='member'
+        to=User, on_delete=models.CASCADE, related_name='member', blank=True
     )
     email = models.EmailField()
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    address = models.ForeignKey(
+        Address,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=10)
 
+    def save(self, *args, **kwargs):
+        try:
+            user = User.objects.get(email=self.email)
+            self.user = user
+        except exceptions.ObjectDoesNotExist:
+            pass
+
+        super().save(*args, **kwargs)
