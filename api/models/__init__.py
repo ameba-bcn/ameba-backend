28821d7123d04cf97cb67a543d3310f5e610c3e7
jon-alamo <jonrivala@gmail.com>
2020-11-17 19:55:13 +0100

    api.models: Address, Member, Membership

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,8 @@
-# Order matters! :)
+# Order matters, also in migrations! :)
+# First this.
 from api.models.user import User
-# from api.models.address import Address
-# from api.models.member import Member
-# from api.models.membership import Membership
+
+# Then this.
+from api.models.address import Address
+from api.models.member import Member
+from api.models.membership import Membership
