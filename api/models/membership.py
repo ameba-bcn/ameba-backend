9f8912b4c77f860dd8017bdae13e6a601426dc9e
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2020-11-24 18:48:23 +0100

    fix: variable datetime model

--- api/models/membership.py
+++ api/models/membership.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 
-DEFAULT_DURATION = timezone.timedelta(365)
+DEFAULT_DURATION = 365
 
 
 class Membership(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    expires = models.DateTimeField(default=timezone.now() + DEFAULT_DURATION)
+    duration = models.IntegerField(default=DEFAULT_DURATION)
+    expires = models.DateTimeField(blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.expires = timezone.now() + DEFAULT_DURATION
+        return super().save(*args, **kwargs)
