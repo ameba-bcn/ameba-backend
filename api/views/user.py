159a3fdc4886588194c0bd3cdfdd36f800ac4d33
jon-alamo <jonrivala@gmail.com>
2020-12-07 16:23:00 +0100

    refactor: removed unused statement

--- api/views/user.py
+++ api/views/user.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from api.serializers.user import UserSerializer
 
