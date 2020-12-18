e64b0f84dc4772702d142b2b58d6748f9f8bf32a
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2020-12-18 19:40:10 +0100

    admin/artist.py: implemented admin panel for artists

--- api/signals.py
+++ api/signals.py
@@ -1,5 +1,5 @@
 from api.groups import create_group_permissions
-from django.db.models.signals import post_save, post_migrate
+from django.db.models.signals import post_save, post_migrate, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
 
