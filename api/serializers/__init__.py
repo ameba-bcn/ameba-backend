8f258355c3353d927cc96aae2f829d6f03651293
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2020-12-18 20:38:06 +0100

    views/artist.py - serializer/artist.py: implemented

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,2 +1,3 @@
 from api.serializers.user import UserSerializer
 from api.serializers.token import DeleteTokenSerializer
+from api.serializers.artist import ArtistListSerializer, ArtistDetailSerializer
---
+++ api/serializers/artist.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from api.models import Artist, Answer, Question
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question']
+
+
+class AnswersSerializers(serializers.ModelSerializer):
+    question = serializers.SlugRelatedField(
+        read_only=True, many=False, slug_field='question'
+    )
+
+    class Meta:
+        model = Answer
+        fields = ['question', 'answer']
+
+
+class ArtistDetailSerializer(serializers.ModelSerializer):
+    answers = AnswersSerializers(many=True, read_only=True)
+
+    class Meta:
+        model = Artist
+        fields = ['id', 'artistic_name', 'biography', 'created', 'image', 'answers']
+        depth = 1
+
+
+class ArtistListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ['id', 'artistic_name', 'biography', 'created', 'image']
