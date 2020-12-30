c3cc453cad034ed6f76da9287213b043c3627a04
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2020-12-30 10:24:07 +0100

    models/item.py: item model finished

--- api/models/item.py
+++ api/models/item.py
@@ -1,44 +1,53 @@
 from django.db import models
+from django.db.models import Sum
 
 
-class ItemFamily(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length='25', unique=True)
     description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
 
-    @property
-    def price_range(self):
-        min_price = min(self.variants, key=lambda x: x.price)
-        max_price = max(self.variants, key=lambda x: x.price)
-        return [min_price, max_price]
+    def get_stock(self):
+        if stock := self.variants.aggregate(Sum('stock'))['stock__sum'] > 0:
+            return stock
+        else:
+            return self.stock
 
-    @property
-    def preview(self):
-        for variant in self.variants:
-            for picture in variant.pictures:
-                if picture.is_preview:
-                    return picture
+
+class ItemTags(models.Model):
+    item = models.ForeignKey(
+        to='Item', related_name='tags', on_delete=models.DO_NOTHING
+    )
+    name = models.CharField('25')
+
+
+class ItemImage(models.Model):
+    item = models.ForeignKey('Item', on_delete=models.DO_NOTHING)
+    image = models.ImageField()
+    active = models.BooleanField()
 
 
 class ItemVariant(models.Model):
-    item_family = models.ForeignKey(
-        to=ItemFamily, on_delete=models.DO_NOTHING, related_name='variants'
-    )
+    category = models.CharField(max_length='25')
     name = models.CharField(max_length='25')
-    price = models.FloatField()
+    item = models.ForeignKey(
+        to='Item', on_delete=models.DO_NOTHING, related_name='variants'
+    )
     stock = models.IntegerField()
-    discount = models.IntegerField(default=25)
     description = models.TextField(max_length=1000, blank=True)
 
     def get_description(self):
         if self.description:
             return self.description
         else:
-            return self.item_family.description
+            return self.item.description
 
 
-class Picture(models.Model):
+class ItemVariantImage(models.Model):
     item_variant = models.ForeignKey(
-        to=ItemVariant, on_delete=models.DO_NOTHING, related_name='pictures'
+        to='ItemVariant', on_delete=models.DO_NOTHING, related_name='pictures'
     )
     image = models.ImageField()
-    is_preview = models.BooleanField()
+    active = models.BooleanField()
+
