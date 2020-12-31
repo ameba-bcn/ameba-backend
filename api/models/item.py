8b9de60b4946ac3cddb4fdba67302b024e8a5877
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2020-12-31 11:13:28 +0100

    Updated models

--- api/models/item.py
+++ api/models/item.py
@@ -1,5 +1,22 @@
+from datetime import timedelta, datetime
+
 from django.db import models
 from django.db.models import Sum
+
+
+EXPIRE_HOURS_BEFORE_EVENT = 1
+EXPIRE_BEFORE_EVENT = timedelta(hours=EXPIRE_HOURS_BEFORE_EVENT)
+
+
+ARTICLE = 'article'
+SUBSCRIPTION = 'subscription'
+EVENT = 'event'
+
+ITEM_TYPES = (
+    (ARTICLE, 'Article'),
+    (SUBSCRIPTION, 'Subscription'),
+    (EVENT, 'Event')
+)
 
 
 class Item(models.Model):
@@ -7,6 +24,21 @@ class Item(models.Model):
     description = models.TextField(max_length=1000)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.IntegerField()
+    type = models.CharField(max_length=25, choices=ITEM_TYPES)
+    date = models.DateTimeField(blank=True)
+    is_expired = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def expire(self):
+        self.is_expired = True
+        self.save()
+
+    def get_is_expired(self):
+        if self.date and datetime.now() > self.date - EXPIRE_BEFORE_EVENT:
+            self.expire()
+        return self.is_expired
 
     def get_stock(self):
         if stock := self.variants.aggregate(Sum('stock'))['stock__sum'] > 0:
@@ -23,6 +55,10 @@ class ItemImage(models.Model):
                              related_name='images')
     image = models.ImageField()
     active = models.BooleanField(default=True)
+
+    @property
+    def url(self):
+        return self.image.url
 
     def __str__(self):
         return self.image.name
--- api/serializers/item.py
+++ api/serializers/item.py
@@ -6,12 +6,38 @@ from api.models import Item
 class ItemDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['name', 'description', 'price', 'stock', 'variants',
+        fields = ['id', 'name', 'description', 'price', 'stock', 'variants',
                   'images']
-        depth = 2
+        depth = 1
 
 
 class ItemListSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+
+    def get_image_list(self, item):
+        request = self.context.get('request')
+        return [request.build_absolute_uri(im.url) for im in item.images.all()]
+
+    def get_price(self, item):
+        request = self.context.get('request')
+        if request.user.is_authenticated:
+            return item.price
+        return item.price
+
+    def get_discount(self, user, item):
+        discounts = [0]
+        for discount in item.discounts.all():
+            # Group discounts
+            for group in discount.groups.all():
+                if group in user.groups.all():
+                    discounts.append(discount.value)
+            # User discounts
+            for user_discount in discount.user_discounts.all():
+                if user_discount.user == user:
+                    discounts.append(discount.value)
+        return max(discounts)
+
     class Meta:
         model = Item
         fields = ['id', 'name', 'price', 'images']
