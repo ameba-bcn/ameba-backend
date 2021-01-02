f8a2ac23d98dc334ed18ca986d5d20e3f80046e2
jon-alamo <jonrivala@gmail.com>
2021-01-02 17:14:43 +0100

    removed events from this pr

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -2,4 +2,3 @@ from api.views.token import TokenView
 from api.views.user import UserViewSet
 from api.views.artist import ArtistViewSet
 from api.views.articles import ArticleViewSet
-from api.views.events import EventViewSet
