a998f2af672b63598507a0657f114f2ce195f939
jon-alamo <jonrivala@gmail.com>
2021-01-02 17:09:30 +0100

    +events endpoint to api

--- api/urls.py
+++ api/urls.py
@@ -16,6 +16,9 @@ router.register(
 router.register(
     r'articles', views.ArticleViewSet, basename='article'
 )
+router.register(
+    r'events', views.EventViewSet, basename='event'
+)
 
 
 urlpatterns = [
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,5 @@
 from api.views.token import TokenView
 from api.views.user import UserViewSet
 from api.views.artist import ArtistViewSet
-from api.views.article import ArticleViewSet
+from api.views.articles import ArticleViewSet
+from api.views.events import EventViewSet
