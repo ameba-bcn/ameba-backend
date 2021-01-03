aee40f18e37ece0bd5d563e7137f96a45a326732
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-01-03 12:29:24 +0100

    implemented admin/interviews.py

--- api/models/artist.py
+++ api/models/artist.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext as _
 class Artist(models.Model):
     name = models.CharField(max_length=50)
     biography = models.TextField(max_length=2500)
+
+    def __str__(self):
+        return self.name
