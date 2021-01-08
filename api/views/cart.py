9995765ff33dc4e6cc3e45994004c993fb16bc80
jon-alamo <jonrivala@gmail.com>
2021-01-08 16:05:19 +0100

    Implemented /carts/current/ to retrieve current user's cart

--- api/views/cart.py
+++ api/views/cart.py
@@ -12,3 +12,22 @@ class CartViewSet(GenericViewSet, RetrieveModelMixin, CreateModelMixin,
     permission_classes = (CartPermission, )
     serializer_class = CartSerializer
     queryset = Cart.objects.all()
+
+    def get_object(self):
+        if self._is_current_user_cart(self.kwargs.get('pk')):
+            current = self._get_current_user_cart(self.request.user)
+            if current:
+                self.kwargs['pk'] = current.pk
+        return super().get_object()
+
+    @staticmethod
+    def _is_current_user_cart(pk):
+        return type(pk) is str and pk == 'current'
+
+    @staticmethod
+    def _get_current_user_cart(user):
+        if user.is_authenticated:
+            if Cart.objects.filter(user=user).exists():
+                return Cart.objects.filter(user=user)[0]
+            else:
+                return Cart.objects.create(user=user)
