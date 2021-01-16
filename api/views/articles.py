730b4e6876c41e75debf83d5c7cec5151ef0a350
jon-alamo <jonrivala@gmail.com>
2021-01-16 13:11:55 +0100

    implemented Events view and serializer

--- api/views/articles.py
+++ api/views/articles.py
@@ -1,9 +1,9 @@
-from api.serializers import ArticleListSerializer, ArticleDetailSerializer
+from api.serializers import EventListSerializer, ArticleDetailSerializer
 from api.models import Article
 from api.views.base import BaseReadOnlyViewSet
 
 
 class ArticleViewSet(BaseReadOnlyViewSet):
-    list_serializer = ArticleListSerializer
+    list_serializer = EventListSerializer
     detail_serializer = ArticleDetailSerializer
     model = Article
