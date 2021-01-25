9b916a643b36a0388f684cf3020905ffb2741b5f
jon-alamo <jonrivala@gmail.com>
2021-01-25 22:22:22 +0100

    admin.py + migrations

---
+++ api/admin/subscription.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from api.admin.item import BaseItemAdmin
+from api.models import Subscription
+
+
+class SubscriptionAdmin(BaseItemAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'description', 'benefits', 'price', 'stock',
+                           'created', 'updated']})
+    ]
+
+
+admin.site.register(Subscription, SubscriptionAdmin)
