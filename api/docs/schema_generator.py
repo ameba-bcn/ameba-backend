6e79a3c6baecbbdc73b5789e7ef4a446ddec21b5
jon-alamo <jonrivala@gmail.com>
2021-02-12 20:43:05 +0100

    Refactored schema_generator.py

--- api/docs/schema_generator.py
+++ api/docs/schema_generator.py
@@ -4,15 +4,16 @@ from api.docs import carts
 
 
 class CustomOpenAPISchemaGenerator(OpenAPISchemaGenerator):
-  def get_schema(self, request=None, public=False):
-    """Generate a :class:`.Swagger` object with custom tags"""
 
-    swagger = super().get_schema(request, public)
-    swagger.tags = [
-        {
-            "name": "carts",
-            "description": carts.__doc__
-        },
-    ]
+    def get_schema(self, request=None, public=False):
+        """Generate a :class:`.Swagger` object with custom tags"""
 
-    return swagger
+        swagger = super().get_schema(request, public)
+        swagger.tags = [
+            {
+                "name": "carts",
+                "description": carts.__doc__
+            },
+        ]
+
+        return swagger
