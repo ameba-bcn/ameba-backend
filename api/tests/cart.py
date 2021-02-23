83d52537cda7d33a26a79b6cd21d4b287e387677
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-02-23 20:53:30 +0100

    tests/cart.py + models.cart.py missing user related name

--- api/tests/cart.py
+++ api/tests/cart.py
@@ -1,7 +1,117 @@
 from api.tests._helpers import BaseTest, check_structure
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from api.models.cart import Cart
+from api.models.user import User
 
 
-class CartTest(BaseTest):
+class CartFlowTests(BaseTest):
+    DETAIL_ENDPOINT = '/api/carts/{pk}/'
+    LIST_ENDPOINT = '/api/carts/'
 
-    def test_cart_with_valid_code_applies_discount(self):
-        pass
+    @staticmethod
+    def get_user(user_tag=None):
+        user_props = {
+            'username': f'User {user_tag or 0}',
+            'password': 'passwordsitomio',
+            'email': f'amebauser_{user_tag or 0}@ameba.cat',
+            'is_active': True
+        }
+        return User.objects.create(**user_props)
+
+    @staticmethod
+    def get_token(user):
+        return RefreshToken.for_user(user)
+
+    @staticmethod
+    def get_cart(user=None, items=None):
+        cart = Cart.objects.create(user=user)
+        if items:
+            cart.items.set(items)
+        return cart
+
+    @staticmethod
+    def check_ownership(cart, user):
+        cart = cart.refresh_from_db()
+        return cart.user == user
+
+    def test_get_no_auth_no_owned_id_cart_returns_200(self):
+        cart = self.get_cart()
+        response = self._get(pk=cart.pk)
+
+        response_struct = {
+            "id": str(cart.id),
+            "user": None,
+            "total": "0.00 €",
+            "count": 0,
+            "cart_items": [],
+            "discount_code": None
+        }
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, response_struct)
+
+    def test_get_no_auth_no_owned_current_cart_returns_401(self):
+        response = self._get(pk='current')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_no_auth_owned_others_id_cart_returns_401(self):
+        user = self.get_user()
+        cart = self.get_cart(user=user)
+        response = self._get(pk=cart.pk)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_auth_no_owned_id_cart_returns_200_and_get_ownership(self):
+        user = self.get_user()
+        token = self.get_token(user)
+        cart = self.get_cart()
+        response = self._get(pk=cart.id, token=token.access_token)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(self.check_ownership(cart, user))
+
+    def test_get_auth_no_owned_current_cart_returns_200_and_get_ownership(self):
+        assert False
+
+    def test_get_auth_owned_id_cart_returns_200(self):
+        assert False
+
+    def test_get_auth_owned_others_id_cart_returns_401(self):
+        assert False
+
+    def test_get_auth_owned_current_cart_returns_200(self):
+        assert False
+
+    def test_create_cart_no_auth(self):
+        assert False
+
+    def test_create_current_cart_auth(self):
+        assert False
+
+    def test_create_cart_auth_returns_current_cart(self):
+        assert False
+
+    def test_create_cart_wrong_auth_gives_401(self):
+        assert False
+
+    def test_patch_cart_items_no_auth(self):
+        assert False
+
+    def test_patch_cart_no_items_list_no_auth_doesnt_change_cart(self):
+        assert False
+
+    def test_patch_cart_empty_items_list_no_auth_empty_cart(self):
+        assert False
+
+    def test_patch_current_cart_auth(self):
+        assert False
+
+    def test_patch_current_cart_empty_items_list_empty_cart(self):
+        assert False
+
+    def test_patch_current_cart_no_items_list_doesnt_change_current_cart(self):
+        assert False
+
+    def test_patch_id_cart_auth_sets_id_cart_as_current(self):
+        assert False
