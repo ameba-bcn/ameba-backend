139a2574d646eb24af11bb7ad697439093bfa11a
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-04-15 19:49:03 +0200

    templates/html_body_templates: upper solved

--- api/signals/emails.py
+++ api/signals/emails.py
@@ -1,14 +1,36 @@
 import django.dispatch
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-from api.email_factories import UserActivationEmailFactory
+from api.email_factories import (
+    UserRegisteredEmail,
+    ActivatedAccountEmail,
+    PasswordChangedEmail,
+    PasswordResetRequestEmail,
+    NewMembershipEmail,
+    PaymentSuccessfulEmail
+)
 
+from api.models import Payment
 
 user_registered = django.dispatch.Signal(providing_args=['user', 'request'])
+account_activated = django.dispatch.Signal(providing_args=['user', 'request'])
+new_member = django.dispatch.Signal(providing_args=['user', 'request'])
 
 
 @receiver(user_registered)
 def on_user_registered(sender, user, request, **kwargs):
-    email_factory = UserActivationEmailFactory.from_request(request, user=user)
-    email = email_factory.create()
+    email = UserRegisteredEmail.from_request(request, user=user)
+    email.send()
+
+
+@receiver(account_activated)
+def on_account_activated(sender, user, request, **kwargs):
+    email = ActivatedAccountEmail.from_request(request, user=user)
+    email.send()
+
+
+@receiver(new_member)
+def on_new_member(sender, user, request, **kwargs):
+    email = NewMembershipEmail.from_request(request, user=user)
     email.send()
