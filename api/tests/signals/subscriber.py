8cc05520b97ef059a7965599d808ba2046b649b2
jon-alamo <jonrivala@gmail.com>
2021-04-25 12:24:50 +0200

    signals/subscriber.py: creates subscribptions on user registered

--- api/tests/signals/subscriber.py
+++ api/tests/signals/subscriber.py
@@ -2,36 +2,32 @@ from unittest import mock
 from django.conf import settings
 
 from api.tests._helpers import BaseTest
-from api.models import MailingList, Subscriber, User
+from api.models import Subscriber, User
 
 
 class SubscriberSignalsTest(BaseTest):
 
-    @mock.patch('api.mailgun.add_member')
-    def test_new_subscription_gets_synced(self, add_member_mock):
+    def test_new_user_gets_subscribed(self):
         email = 'user@email.com'
-        address = 'new_mailing_list@mail-out.ameba.cat'
-        mailing_list = MailingList.objects.create(address=address)
-        subscriber = Subscriber.objects.create(email=email)
-        subscriber.mailing_lists.add(mailing_list)
-        add_member_mock.assert_called_with(email=email, list_address=address)
 
-    @mock.patch('api.mailgun.remove_member')
-    def test_unsubscription_gets_synced(self, remove_member_mock):
-        email = 'user@email.com'
-        address = 'new_mailing_list@mail-out.ameba.cat'
-        mailing_list = MailingList.objects.create(address=address)
-        subscriber = Subscriber.objects.create(email=email)
-        subscriber.mailing_lists.add(mailing_list)
-        subscriber.mailing_lists.remove(mailing_list)
-        remove_member_mock.assert_called_with(email=email,list_address=address)
+        self.assertFalse(Subscriber.objects.filter(email=email))
 
-    @mock.patch('api.mailgun.add_member')
-    def test_new_user_gets_subscribed_to_default_list(self, add_member_mock):
-        email = 'new_user@ameba.cat'
         User.objects.create(
-            email=email, username='new_user', password='123456'
+            email=email, username='user', password='12345'
         )
-        add_member_mock.assert_called_with(
-            email=email, list_address=settings.DEFAULT_MAILING_LIST
+
+        self.assertTrue(Subscriber.objects.filter(email=email))
+        subscriber = Subscriber.objects.get(email=email)
+        self.assertTrue(subscriber.mailing_lists.filter(
+            address=settings.DEFAULT_MAILING_LIST)
         )
+
+    def test_removed_user_gets_unsubscribed(self):
+        email = 'user@email.com'
+        self.assertFalse(Subscriber.objects.filter(email=email))
+        user = User.objects.create(
+            email=email, username='user', password='12345'
+        )
+        self.assertTrue(Subscriber.objects.filter(email=email))
+        user.delete()
+        self.assertFalse(Subscriber.objects.filter(email=email))
