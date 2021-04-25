559ce669e1a6fd5d5f91ab4e4637a383273366d1
jon-alamo <jonrivala@gmail.com>
2021-04-25 12:13:02 +0200

    signals/subscriber.py: new subscriber on register

--- api/tests/signals/subscriber.py
+++ api/tests/signals/subscriber.py
@@ -1,7 +1,8 @@
 from unittest import mock
+from django.conf import settings
 
 from api.tests._helpers import BaseTest
-from api.models import MailingList, Subscriber
+from api.models import MailingList, Subscriber, User
 
 
 class SubscriberSignalsTest(BaseTest):
@@ -24,3 +25,13 @@ class SubscriberSignalsTest(BaseTest):
         subscriber.mailing_lists.add(mailing_list)
         subscriber.mailing_lists.remove(mailing_list)
         remove_member_mock.assert_called_with(email=email,list_address=address)
+
+    @mock.patch('api.mailgun.add_member')
+    def test_new_user_gets_subscribed_to_default_list(self, add_member_mock):
+        email = 'new_user@ameba.cat'
+        User.objects.create(
+            email=email, username='new_user', password='123456'
+        )
+        add_member_mock.assert_called_with(
+            email=email, list_address=settings.DEFAULT_MAILING_LIST
+        )
