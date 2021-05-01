4b0c855f7f5e1153e7a2c954b0031202fe3fd753
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-05-01 18:37:26 +0200

    views/article.py, event.py, subscription.py: updated with new serializers

--- api/views/articles.py
+++ api/views/articles.py
@@ -1,17 +1,11 @@
-# from api.serializers import ArticleListSerializer, ArticleDetailSerializer
-# from api.models import Article
-# from api.views.base import BaseReadOnlyViewSet
-#
-#
-# class ArticleViewSet(BaseReadOnlyViewSet):
-#     list_serializer = ArticleListSerializer
-#     detail_serializer = ArticleDetailSerializer
-#     model = Article
-#     queryset = Article.objects.filter(is_active=True)
-#
-#
-# class ArticleFamilyViewSet(BaseReadOnlyViewSet):
-#     list_serializer = ArticleListSerializer
-#     detail_serializer = ArticleDetailSerializer
-#     model = Article
-#     queryset = Article.objects.filter(is_active=True)
+from api.serializers import ArticleDetailSerializer, ArticleListSerializer
+from api.models import Article
+from api.views.base import BaseReadOnlyViewSet
+
+
+class ArticleViewSet(BaseReadOnlyViewSet):
+    list_serializer = ArticleListSerializer
+    detail_serializer = ArticleDetailSerializer
+    model = Article
+    queryset = Article.objects.filter(is_active=True)
+
