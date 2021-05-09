133554d2d665c01c0e3742a9035c79cb4c9f38d8
jon-alamo <jonrivala@gmail.com>
2021-05-09 14:20:48 +0200

    api/docs/user.py: updated user doc

--- api/views/user.py
+++ api/views/user.py
@@ -59,7 +59,7 @@ class UserViewSet(
             user_registered.send(sender=User, user=user, request=request)
         return response
 
-    @action(detail=True, serializer_class=serializers.MemberSerializer)
+    @action(detail=True, serializer_class=serializers.DocMemberSerializer)
     def member_profile(self, request, *args, **kwargs):
         user = self.get_object()
         if not user.has_member_profile():
