937e3fbf54f2f08e2828bba9137031a7153481e3
jon-alamo <jonrivala@gmail.com>
2021-05-10 20:19:21 +0200

    signals/membership.py: create membership on new subscription purchase

--- api/signals/memberships.py
+++ api/signals/memberships.py
@@ -10,4 +10,9 @@ subscription_purchased = dispatch.Signal(
 
 @dispatch.receiver(signals.post_save, sender=Payment)
 def on_new_subscription(member, subscription):
-    pass
+    active_ms = Membership.objects.filter(member=member).orderby(
+        '-expires').first()
+    attrs = dict(member=member, subscription=subscription)
+    if not active_ms.is_expired():
+        attrs['starts'] = active_ms.expires
+    Membership.objects.create(**attrs)
