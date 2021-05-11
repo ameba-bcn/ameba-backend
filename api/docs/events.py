94687c3f9b54f8f45cecde56b2c9ffd7cdc005a9
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-05-11 09:55:31 +0200

    bugfix: serializers/subscription.py: removed benefits from subscription

--- api/docs/events.py
+++ api/docs/events.py
@@ -2,7 +2,7 @@
 
 class EventsDocs:
     common = """
-# ARTICLES
+# EVENTS
 Endpoint para los eventos de la agenda. El objeto Event, al igual que 
 Article y Subscription hereda de Item, por lo que comparten numeración única 
 de ids. Es decir, un Article un Event y una Subscription nunca pueden 
