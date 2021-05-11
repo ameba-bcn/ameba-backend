473c164b5f2daeb65fbbec834c8a57fb9beeed7b
jon-alamo <jonrivala@gmail.com>
2021-05-11 18:12:06 +0200

    settings.py: updated release

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 from envs import env
 import os
 
-VERSION = '0.16'
+VERSION = '0.17'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
