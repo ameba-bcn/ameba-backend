313b6c7233ce870ac9eec232de7a43cb0fc74a79
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-05-17 21:08:40 +0200

    serializers/user.py: added optional cart_id attribute

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
+
+from api import models
+from api import exceptions
 
 # Get current user model
 User = get_user_model()
@@ -8,14 +12,28 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     member = serializers.PrimaryKeyRelatedField(read_only=True)
     date_joined = serializers.DateTimeField(required=False)
+    cart_id = serializers.CharField(max_length=64, write_only=True)
 
     class Meta:
         model = User
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
         fields = [
-            'username', 'password', 'email', 'member', 'date_joined'
+            'username', 'password', 'email', 'member', 'date_joined', 'cart_id'
         ]
 
     def validate(self, attrs):
         attrs = super().validate(attrs)
         return attrs
+
+    def create(self, validated_data):
+        cart_id = validated_data.pop('cart_id', None)
+        user = super().create(validated_data)
+        if models.Cart.objects.filter(id=cart_id):
+            cart = models.Cart.objects.get(id=cart_id)
+            cart.user = user
+            cart.save()
+        elif cart_id:
+            raise exceptions.WrongCartId
+        return user
