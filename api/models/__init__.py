0b62d921866f19aff81e1bdd68fc5cd1390e6b1b
jon-alamo <jonrivala@gmail.com>
2021-05-27 19:17:12 +0200

    bugfix: group mandatory for subscriptions and fixtures adapted

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -8,6 +8,7 @@ from api.models.mailing_list import MailingList
 from api.models.covers import Cover
 from api.models.images import Image
 from api.models.item import Item, ItemVariant, ItemAttribute, ItemAttributeType
+from api.models.subscription import Subscription
 from api.models.member import Member
 from api.models.membership import Membership
 from api.models.artist import Artist, ArtistMediaUrl
@@ -16,6 +17,5 @@ from api.models.article import Article
 from api.models.discount import Discount, DiscountCode
 from api.models.cart import Cart, CartItems
 from api.models.event import Event
-from api.models.subscription import Subscription
 from api.models.payment import Payment
 from api.models.subscriber import Subscriber
