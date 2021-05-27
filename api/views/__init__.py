691ae8ea18a0b8d2e74afa91757d0c001fc143bd
jon-alamo <jonrivala@gmail.com>
2021-05-27 19:20:28 +0200

    bugfix: uncommented code

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,17 +1,17 @@
-# from api.views.token import TokenView
-# from api.views.user import UserViewSet
-# from api.views.interview import InterviewViewSet
-# from api.views.articles import ArticleViewSet
-# from api.views.cart import CartViewSet
-# from api.views.event import EventViewSet
-# from api.views.event import UserSavedEventsViewSet
-# from api.views.subscription import SubscriptionViewSet
-# from api.views.activate import activate
-# from api.views.version import current_version
-# from api.views.templates import mail_template
-# from api.views.recovery import RecoveryViewSet
-# from api.views.covers import CoversViewSet
-# from api.views.artists import ArtistViewSet
-# from api.views.subscriber import subscribe, mailgun_unsubscribe_hook
-# from api.views.member_register import member_register
-# from api.views.about import about
+from api.views.token import TokenView
+from api.views.user import UserViewSet
+from api.views.interview import InterviewViewSet
+from api.views.articles import ArticleViewSet
+from api.views.cart import CartViewSet
+from api.views.event import EventViewSet
+from api.views.event import UserSavedEventsViewSet
+from api.views.subscription import SubscriptionViewSet
+from api.views.activate import activate
+from api.views.version import current_version
+from api.views.templates import mail_template
+from api.views.recovery import RecoveryViewSet
+from api.views.covers import CoversViewSet
+from api.views.artists import ArtistViewSet
+from api.views.subscriber import subscribe, mailgun_unsubscribe_hook
+from api.views.member_register import member_register
+from api.views.about import about
