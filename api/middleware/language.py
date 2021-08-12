8f6d280ae59ee557e361ad937acd71154f81f8c6
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-08-12 18:30:46 +0200

    middleware/language.py: get authenticated user with simple jwt in middleware

--- api/middleware/language.py
+++ api/middleware/language.py
@@ -1,8 +1,10 @@
 from django.utils import translation
+from rest_framework_simplejwt import authentication
 
 
 def user_based_language_middleware(get_response):
     def middleware(request):
+        request.user = authentication.JWTAuthentication().authenticate(request)[0]
         user = getattr(request, 'user', None)
         lang = translation.get_language()
         if user.is_authenticated and user.language != lang:
