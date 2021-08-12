5599949152dae3117403311a4b492f48331a3575
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-08-12 18:16:36 +0200

    middleware/language.py: set preffered language from requestest lang

--- api/middleware/language.py
+++ api/middleware/language.py
@@ -4,10 +4,10 @@ from django.utils import translation
 def user_based_language_middleware(get_response):
     def middleware(request):
         user = getattr(request, 'user', None)
-
-        if user is not None and user.is_authenticated and user.language:
-            translation.activate(user.language)
+        lang = translation.get_language()
+        if user.is_authenticated and user.language != lang:
+            user.language = lang
+            user.save()
         response = get_response(request)
-        translation.deactivate()
         return response
     return middleware
