85a15e0b97669f552d3d2065467ae101f9186bfd
jon-alamo <jonrivala@gmail.com>
2021-08-23 17:45:09 +0200

    feature -> backend/urls.py: deactivated middleware to set language and prefix from url in api

--- api/middleware/language.py
+++ api/middleware/language.py
@@ -3,6 +3,9 @@ from rest_framework_simplejwt import authentication
 
 
 def user_based_language_middleware(get_response):
+    """ DO NOT USE. Only client will determine language of provided
+    content.
+    """
     def middleware(request):
         auth = authentication.JWTAuthentication().authenticate(request)
         if auth:
--- backend/settings.py
+++ backend/settings.py
@@ -84,8 +84,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'api.middleware.user_based_language_middleware'
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
