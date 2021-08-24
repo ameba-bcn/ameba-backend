a09c65bb8e53787b674fc8903b9ffa99600ae99b
jon-alamo <jonrivala@gmail.com>
2021-08-24 19:49:25 +0200

    models/artist.py: added __str__ to ArtistTag

--- api/models/artist.py
+++ api/models/artist.py
@@ -5,7 +5,10 @@ BIO_PREVIEW = 160
 
 
 class ArtistTag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class ArtistMediaUrl(models.Model):
