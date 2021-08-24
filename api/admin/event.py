9688bd0f9ffe0c9ba8b25d65d70560fa159070a5
jon-alamo <jonrivala@gmail.com>
2021-08-24 18:37:08 +0200

    admin/event.py: added tag to Event admin view

--- api/admin/event.py
+++ api/admin/event.py
@@ -4,8 +4,16 @@ from api.admin.item import BaseItemAdmin
 from api.models import Event
 
 
+from api.models import Event, EventTag
+
+
+class EventTagAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+
 class EventAdmin(BaseItemAdmin):
-    fields = BaseItemAdmin.fields + ['datetime', 'address', 'artists']
+    fields = BaseItemAdmin.fields + ['datetime', 'address', 'artists', 'tag']
 
 
 admin.site.register(Event, EventAdmin)
+admin.site.register(EventTag, EventTagAdmin)
