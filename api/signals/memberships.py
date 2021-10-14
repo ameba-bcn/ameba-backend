ab7ed7ab0bfb13a3ab70385222339d4669701e39
jon-alamo <jonrivala@gmail.com>
2021-10-14 20:07:27 +0200

    wip: trigger_notifications signal

--- api/signals/memberships.py
+++ api/signals/memberships.py
@@ -2,7 +2,9 @@ from django import dispatch
 import django.db.models.signals as signals
 
 from api.models import Membership
-from api.tasks.memberships import schedule_reminders
+from api.bg_tasks.notifications import (
+    notify_member_about_to_expire, notify_member_expired
+)
 from api.signals import new_member
 
 subscription_purchased = dispatch.Signal(
@@ -22,9 +24,11 @@ def create_membership(sender, member, subscription, **kwargs):
 
 
 @dispatch.receiver(signals.post_save, sender=Membership)
-def trigger_notifications(sender, member, subscription, **kwargs)
-    # Trigger reminders
-    schedule_reminders(schedule=0)
+def trigger_notifications(sender, member, created, **kwargs):
+    if created:
+        # Trigger reminders
+        notify_member_about_to_expire(schedule=0)
+        notify_member_expired(schedule=0)
 
-    # Trigger new member signal
-    new_member.send(sender=Membership)
+        # Trigger new member signal
+        new_member.send(sender=Membership)
--- api/tasks/memberships.py
+++
@@ -1,6 +0,0 @@
-from background_task import background
-
-
-@background(schedule=0)
-def schedule_reminders(membership):
-    pass
