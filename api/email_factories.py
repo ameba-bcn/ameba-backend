a4e9bb620a0b08f6fe41c607117cdbc6de3facb8
jon-alamo <jonrivala@gmail.com>
2021-10-29 12:40:33 +0200

    api/signals/emails.py: added context to every email

--- api/email_factories.py
+++ api/email_factories.py
@@ -1,6 +1,5 @@
 import django.utils.encoding as encoding
 import django.core.mail as mail
-import django.contrib.sites.shortcuts as shortcuts
 import django.conf as conf
 import django.utils.http as http
 import django.template.loader as loader
@@ -23,68 +22,38 @@ class UserEmailFactoryBase(object):
     plain_body_template = None
     html_body_template = None
 
-    def __init__(self, mail_to=None, user=None, request=None, **context):
-        assert mail_to or user or (request and request.user.email)
-        self.mail_to = user and user.email or mail_to
-        self.user = self.get_user(user, request)
+    def __init__(self, mail_to, **context):
+        self.mail_to = mail_to
         self.from_email = conf.settings.DEFAULT_FROM_EMAIL
-        self.request = request
         self.context = context
-        self.token_url = self.get_token_url(self.user)
-        self.site = self.get_site(request)
-        self.context.update({'site': self.site, 'token_url': self.token_url})
-        self.email_message = self.create()
+        self.email_message = self.create(
+            self.plain_body_template,
+            self.html_body_template,
+            self.subject_template,
+            self.context,
+            self.mail_to,
+            self.from_email
+        )
 
     def send(self):
         return self.email_message.send()
 
-    @staticmethod
-    def get_user(user, request):
-        if user:
-            return user
-        elif request:
-            return request.user
-
-    @staticmethod
-    def get_token_url(user):
-        if user:
-            token = user_token_generator(user)
-            return conf.settings.ACTIVATION_URL.format(token=token)
-
-    @staticmethod
-    def get_site(request):
-        if request:
-            return shortcuts.get_current_site(request)
-        return conf.settings.HOST_NAME
-
     @classmethod
-    def send_to(cls, mail_to=None, user=None, **context):
-        assert mail_to or user
-        email_object = cls(mail_to=mail_to, user=user, **context)
+    def send_to(cls, mail_to, **context):
+        email_object = cls(mail_to=mail_to, **context)
         return email_object.send()
 
-    @classmethod
-    def from_request(cls, request, user=None, **context):
-        factory_object = cls(
-            request=request,
-            user=user,
-            **context
-        )
-        return factory_object.create()
-
-    def create(self):
-        assert self.plain_body_template and self.html_body_template
-        context = self.context
-        subject = loader.render_to_string(self.subject_template, context)
+    @staticmethod
+    def create(plain_body, html_body, subject, context, mail_to, from_email):
+        assert plain_body and html_body and subject
+        subject = loader.render_to_string(subject, context)
         subject = ''.join(subject.splitlines())
-
-        plain_body = loader.render_to_string(self.plain_body_template, context)
+        plain_body = loader.render_to_string(plain_body, context)
         email_message = mail.EmailMultiAlternatives(
-            subject, plain_body, self.from_email, [self.mail_to]
+            subject, plain_body, from_email, [mail_to]
         )
-        html_body = loader.render_to_string(self.html_body_template, context)
+        html_body = loader.render_to_string(html_body, context)
         email_message.attach_alternative(html_body, "text/html")
-
         return email_message
 
 
