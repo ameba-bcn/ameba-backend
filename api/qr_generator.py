fada34f90be4fd9620d753f39305bf3cf3b6991d
jon-alamo <jonrivala@gmail.com>
2021-11-01 12:11:19 +0100

    models/member.py: create qr token for member card

--- api/qr_generator.py
+++ api/qr_generator.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 import qrcode
 
@@ -14,3 +15,11 @@ def create_qr_code(url, qr_id):
     qr_path = os.path.join(QR_DIR, f'{qr_id}.png')
     img.save(qr_path)
     return qr_path
+
+
+def generate_member_card_qr(member, protocol, site_name):
+    token = member.get_member_card_token()
+    url_path = f'ameba-site/?token={token}'
+    url = urllib.parse.urljoin(f'{protocol}://{site_name}', url_path)
+    qr_code_path = create_qr_code(url, member.id)
+    return qr_code_path
