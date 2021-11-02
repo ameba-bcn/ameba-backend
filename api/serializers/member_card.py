3b10ea9c4b9dedc61c8189d773bfc26e1ef6e9b6
jon-alamo <jonrivala@gmail.com>
2021-11-02 21:09:51 +0100

    api/serializers/member_card.py: fixed bugs

--- api/serializers/member_card.py
+++ api/serializers/member_card.py
@@ -24,23 +24,23 @@ class MemberCardSerializer(SingleUseTokenSerializer):
     last_name = serializers.SerializerMethodField(read_only=True)
     expires = serializers.SerializerMethodField(read_only=True)
 
-    def get_number(self):
-        return self.obj.number()
+    def get_number(self, *args, **kwargs):
+        return self.obj.number
 
-    def get_subscription(self):
-        return self.obj.get_newest_membership().name
+    def get_subscription(self, *args, **kwargs):
+        return self.obj.get_newest_membership().subscription.name
 
-    def get_status(self):
+    def get_status(self, *args, **kwargs):
         return self.obj.status
 
-    def get_username(self):
+    def get_username(self, *args, **kwargs):
         return self.obj.user.username
 
-    def get_first_name(self):
+    def get_first_name(self, *args, **kwargs):
         return self.obj.first_name
 
-    def get_last_name(self):
+    def get_last_name(self, *args, **kwargs):
         return self.obj.last_name
 
-    def get_expires(self):
+    def get_expires(self, *args, **kwargs):
         return self.obj.get_newest_membership().expires
