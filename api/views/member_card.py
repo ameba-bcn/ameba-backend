0a582ddc3584c6c13550b99d40b1e311f187f24a
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-11-02 22:48:24 +0100

    api/docs added membercard docs

--- api/views/member_card.py
+++ api/views/member_card.py
@@ -1,24 +1,12 @@
-from rest_framework.decorators import api_view, authentication_classes, \
-    permission_classes
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from api.serializers import MemberCardSerializer
 from api.responses import MemberCardResponse
-from api.views.base import BaseReadOnlyViewSet
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
 
 from api import authentication
 from api import models
-
-@swagger_auto_schema(method='post', request_body=MemberCardSerializer)
-@api_view(['POST'])
-@authentication_classes((authentication.MemberCardAuthentication, ))
-@permission_classes((IsAuthenticated, ))
-def member_card(request):
-    member = request.user.member
-    serializer = MemberCardSerializer(instance=member)
-    return MemberCardResponse(serializer.data)
+from api.docs.member_card import MemberCardDocs
 
 
 class MemberCard(ListModelMixin, GenericViewSet):
@@ -31,3 +19,5 @@ class MemberCard(ListModelMixin, GenericViewSet):
         member = request.user.member
         serializer = MemberCardSerializer(instance=member)
         return MemberCardResponse(serializer.data)
+
+    list.__doc__ = MemberCardDocs.list
