2c5c50a86ad29f7efb6dad7f6c2950955fa69284
jon-alamo <jonrivala@gmail.com>
2021-11-11 19:33:42 +0100

    views/event_ticket.py: created view to access event ticket

--- api/serializers/event_ticket.py
+++ api/serializers/event_ticket.py
@@ -9,10 +9,34 @@ User = get_user_model()
 
 
 class EventTicketSerializer(ModelSerializer):
+    event = serializers.SlugRelatedField(
+        source='item_variant.item', slug_field='name', read_only=True
+    )
+    variants = serializers.SerializerMethodField()
+    user_name = serializers.SlugRelatedField(
+        source='user', slug_field='username'
+    )
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
 
     class Meta:
         model = ItemVariant.acquired_by.through
-        # fields = ('number', 'first_name', 'last_name', 'identity_card',
-        #           'status', 'type', 'username', 'expires')
-        # read_only_fields = fields
+        fields = ('user_name', 'first_name', 'last_name', 'event',
+                  'variants', )
 
+    @staticmethod
+    def get_first_name(item_variant_user):
+        if item_variant_user.user.has_member_profile():
+            return item_variant_user.user.member.first_name
+
+    @staticmethod
+    def get_last_name(item_variant_user):
+        if item_variant_user.user.has_member_profile():
+            return item_variant_user.user.member.last_name
+
+    @staticmethod
+    def get_variants(item_variant_user):
+        return ' '.join(
+            [f'{attribute.attribute.name}: {attribute.value}' for attribute in
+                item_variant_user.item_variant.attributes.all()]
+        )
