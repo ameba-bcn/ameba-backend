0598b714b2a160d54179d297205583f936657108
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-11-25 20:47:29 +0100

    api/stripe.py: create product and prices

--- api/signals/items.py
+++ api/signals/items.py
@@ -4,13 +4,14 @@ import django.db.models.signals as signals
 import api.signals.events as event_signals
 import api.signals.memberships as membership_signals
 import api.models as api_models
+import api.stripe as stripe
 
 items_acquired = django.dispatch.Signal(providing_args=['cart', 'request'])
 
 
 @django.dispatch.receiver(signals.m2m_changed,
                           sender=api_models.ItemVariant.acquired_by.through)
-def process_adquired_items(instance, pk_set, action, model, **kwargs):
+def process_acquired_items(instance, pk_set, action, model, **kwargs):
     if action == 'post_add':
         for user_id in pk_set:
             user = api_models.User.objects.get(id=user_id)
@@ -34,3 +35,9 @@ def process_cart_items(sender, cart, request, **kwargs):
     for cart_item in cart.get_cart_items():
         item_variant = cart_item.item_variant
         item_variant.acquired_by.add(cart.user)
+
+
+@django.dispatch.receiver(signals.post_save, sender=api_models.ItemVariant)
+def add_product_to_stripe(sender, instance, created, **kwargs):
+    if created:
+        stripe
