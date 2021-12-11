0ac3027d0ae0aecf9d67de9c39ad12626ba35e2e
jon-alamo <jonrivala@gmail.com>
2021-12-11 12:33:25 +0100

    refactor -> payments.py: restructured payment confirmation email section

--- api/views/cart.py
+++ api/views/cart.py
@@ -4,7 +4,7 @@ from rest_framework.mixins import (
     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, CreateModelMixin
 )
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import NotAuthenticated
+from rest_framework.exceptions import NotAuthenticated, MethodNotAllowed
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.exceptions import MethodNotAllowed
@@ -83,6 +83,9 @@ class CartViewSet(GenericViewSet, RetrieveModelMixin, UpdateModelMixin,
             request=self.request
         )
         return super().destroy(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed
 
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
