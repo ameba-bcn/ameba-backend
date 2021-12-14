faab38020afada56baa8138afab41561f6778329
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-12-14 09:06:39 +0100

    wip: test cases for new payment flow

--- api/views/stripe.py
+++ api/views/stripe.py
@@ -2,7 +2,7 @@ import rest_framework.decorators as decorators
 import rest_framework.response as response
 
 import stripe
-
+import api.models as api_models
 
 # This is your Stripe CLI webhook secret for testing your endpoint locally.
 endpoint_secret = 'whsec_YXcUnODFG0oV6i0E2lzYV1yVbX9hgbTA'
@@ -23,11 +23,22 @@ def webhook(request):
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
         raise e
+    resp = {'details': 'Not processed'}
 
     if event['type'] == 'invoice.payment_failed':
         invoice = event['data']['object']
+
     elif event['type'] == 'invoice.payment_succeeded':
         invoice = event['data']['object']
+        payments = api_models.Payment.objects.filter(invoice_id=invoice['id'])
+        if payments.exists():
+            payment = payments[0]
+            closed = payment.close_payment()
+            resp = {
+                'details': closed and 'Payment successful' or 'Payment unsuccessful'
+            }
+        else:
+            resp = {'details': 'Payment doesn\'t exist'}
 
-    return response.Response()
+    return response.Response(data=resp)
 
