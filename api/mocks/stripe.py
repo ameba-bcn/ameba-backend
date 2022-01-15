7852b0758d58382fa0eaa0fc2109459d8a814ae0
jon-alamo <jonrivala@gmail.com>
2022-01-15 09:39:42 +0100

    api/mocks/stripe.py: implemented stripe module mock

--- api/mocks/stripe.py
+++ api/mocks/stripe.py
@@ -1,31 +1,57 @@
 import sys
 import types
 import unittest.mock as mock
-import django.conf as conf
+import logging
+import stripe
+import time
 
 this_module = sys.modules[__name__]
 
-module_name = 'stripe'
-module = types.ModuleType(module_name)
-sys.modules[module_name] = module
+# module_name = 'stripe'
+# stripe = types.ModuleType(module_name)
+# sys.modules[module_name] = stripe
+
+
+class Errors:
+    InvalidRequestError = Exception
+    SignatureVerificationError = Exception
 
 
 class BaseMock:
     objects = {}
 
     def __init__(self, id, **kwargs):
-        self.id = id
+        self.id = str(id)
+        self.created = time.time()
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     @classmethod
     def retrieve(cls, id):
-        return cls.objects[id]
+        try:
+            return cls.objects[id]
+        except KeyError:
+            raise Errors.InvalidRequestError
+
+    @classmethod
+    def list(cls, **kwargs):
+        result = {'data': []}
+        for element in cls.objects.values():
+            for key, value in kwargs.items():
+                if not hasattr(element, key) or getattr(element, key).id != value:
+                    break
+            else:
+                result['data'].append(element)
+        return result
 
     @classmethod
     def create(cls, id=None, **kwargs):
         if not id:
-            id = len(cls.objects) + 1
+            if not cls.objects:
+                id = 0
+            else:
+                id = max(map(lambda x: int(x), cls.objects.keys())) + 1
+                id = str(id)
         cls.objects[id] = cls(id=id, **kwargs)
         return cls.retrieve(id=id)
 
@@ -42,12 +68,14 @@ class BaseMock:
 
 
 class ProductMock(BaseMock):
+    objects = {}
 
     def __init__(self, id, name, active=True):
         super().__init__(id=id, name=name, active=active)
 
 
 class PriceMock(BaseMock):
+    objects = {}
 
     def __init__(self, id, currency, product, unit_amount, recurring):
         self.product_id = product
@@ -56,12 +84,14 @@ class PriceMock(BaseMock):
 
 
 class CustomerMock(BaseMock):
+    objects = {}
 
     def __init__(self, id, name):
         super().__init__(id=id, name=name)
 
 
 class SubscriptionMock(BaseMock):
+    objects = {}
 
     def __init__(self, id, customer, items, payment_behaviour):
         self.customer_id = customer
@@ -69,6 +99,7 @@ class SubscriptionMock(BaseMock):
 
 
 class InvoiceItemMock(BaseMock):
+    objects = {}
 
     def __init__(self, id, customer, price):
         self.price_id = price
@@ -76,17 +107,44 @@ class InvoiceItemMock(BaseMock):
 
 
 class InvoiceMock(BaseMock):
+    objects = {}
 
     def __init__(self, id, customer, collection_method):
         self.customer_id = customer
+        self.status = 'draft'
         super().__init__(id=id, collection_method=collection_method)
 
+    def finalize_invoice(self):
+        self.status = 'open'
 
-module.Product = ProductMock
-module.Price = PriceMock
-module.Customer = CustomerMock
-module.Subscription = SubscriptionMock
-module.InvoiceItem = InvoiceItemMock
-module.Invoice = InvoiceMock
-module.PaymentMethod = mock.MagicMock()
-module.PaymentIntent = mock.MagicMock()
+
+class WebhookMock:
+
+    @staticmethod
+    def construct_event(payload, *args, **kwargs):
+        return payload
+
+
+def mock_stripe_succeeded_payment(client, url, invoice):
+    headers = {'HTTP_STRIPE_SIGNATURE': 'stripe-signature'}
+    invoice['status'] = 'paid'
+    data = {
+        'type': 'invoice.payment_succeeded',
+        'data': {
+            'object': invoice
+        }
+    }
+    return client.post(url, data=data, **headers)
+
+
+logging.info('Mocking stripe module ...')
+stripe.Product = ProductMock
+stripe.Price = PriceMock
+stripe.Customer = CustomerMock
+stripe.Subscription = SubscriptionMock
+stripe.InvoiceItem = InvoiceItemMock
+stripe.Invoice = InvoiceMock
+stripe.PaymentMethod = mock.MagicMock()
+stripe.PaymentIntent = mock.MagicMock()
+stripe.Webhook = WebhookMock
+stripe.error = Errors
