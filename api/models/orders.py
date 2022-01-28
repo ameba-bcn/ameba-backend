8b699288dc464db1aeb7632731c05126c099bcf6
jon-alamo <jonrivala@gmail.com>
2022-01-28 21:25:20 +0100

    api/templates/html_body_templates/new_order_internal.html: templates created

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,12 +1,8 @@
-import uuid
-
 from django.utils.translation import gettext as _
 from django.db import models
-from django.db.models import UUIDField
-import api.stripe as api_stripe
+from django.conf import settings
 
-from api.signals.emails import payment_closed
-import api.signals.items as items_signals
+from api.signals.emails import new_order, order_ready
 
 
 class Order(models.Model):
@@ -18,12 +14,13 @@ class Order(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE,
                              verbose_name=_('user'), null=True)
     item_variants = models.ManyToManyField(to='ItemVariant', blank=False)
+    address = models.CharField(max_length=1000, default=settings.ORDERS_ADDRESS)
     ready = models.BooleanField(default=False)
     delivered = models.BooleanField(default=False)
 
     def send_new_order_notification(self):
-        pass
+        new_order.send(self.__class__, self)
 
     def send_order_ready_notification(self):
-        pass
+
 
