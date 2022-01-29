20d4b1b0023edd686ae450afdf7f45fcfc2710c7
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2022-01-29 18:18:53 +0100

    api/migrations/0057_auto_20220129_1718.py: added

--- api/models/orders.py
+++ api/models/orders.py
@@ -8,15 +8,21 @@ from api.signals.emails import new_order, order_ready
 class Order(models.Model):
 
     class Meta:
-        verbose_name = _('Payment')
-        verbose_name_plural = _('Payments')
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
 
     user = models.ForeignKey('User', on_delete=models.CASCADE,
                              verbose_name=_('user'), null=True)
     item_variants = models.ManyToManyField(to='ItemVariant', blank=False)
-    address = models.CharField(max_length=1000, default=settings.ORDERS_ADDRESS)
-    ready = models.BooleanField(default=False)
-    delivered = models.BooleanField(default=False)
+    address = models.CharField(
+        max_length=1000,
+        default=settings.ORDERS_ADDRESS,
+        verbose_name=_('address')
+    )
+    ready = models.BooleanField(default=False, verbose_name=_('ready'))
+    delivered = models.BooleanField(default=False, verbose_name=_('delivered'))
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -28,7 +34,7 @@ class Order(models.Model):
     def send_order_ready_notification(self):
         order_ready.send(self.__class__, self)
 
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
+    def save(self, *args, **kwargs):
         if not self.was_ready and self.ready:
-            pass
+            self.send_order_ready_notification()
+        return super().save(*args, **kwargs)
