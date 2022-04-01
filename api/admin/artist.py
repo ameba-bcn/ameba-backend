cb5ee348ecc9c49263a5c7ef0fdf3408591c6bb8
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2022-04-01 21:47:29 +0200

    Added field to artist media

--- api/admin/artist.py
+++ api/admin/artist.py
@@ -15,7 +15,7 @@ class MediaUrlsInLine(admin.StackedInline):
     model = ArtistMediaUrl
     verbose_name = 'ArtistMediaUrl'
     verbose_name_plural = "ArtistMediaUrls"
-    fields = ('embedded', 'created')
+    fields = ('embedded', 'url', 'created')
     readonly_fields = ('created', )
     extra = 0
 
