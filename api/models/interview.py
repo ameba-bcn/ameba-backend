3399742933360b86f0338768ba6023f50acd1d92
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2022-07-06 21:03:03 +0200

    api/serializers/interview.py: sorting answers by Question.position

--- api/models/interview.py
+++ api/models/interview.py
@@ -41,14 +41,14 @@ class Question(models.Model):
         verbose_name_plural = _('Questions')
 
     question = models.TextField(max_length=2000, verbose_name=_('question'))
-    position = models.IntegerField(blank=True)
+    position = models.IntegerField(null=True, blank=True)
     is_default = models.BooleanField(default=False, verbose_name=_('is default'))
 
     def __str__(self):
         return f'{self.question}'
 
     def save(self, *args, **kwargs):
-        same_pos = self.objects.filter(order=self.position)
+        same_pos = Question.objects.filter(position=self.position)
         if same_pos.exists() and self.position is not None:
             raise ValidationError(message=_(
                 f'There\'s another question with position {self.position}'
