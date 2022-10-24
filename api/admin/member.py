9f2a6828e8e04659b8269696e759343b39011f95
jon-alamo <jonrivala@gmail.com>
2022-10-24 12:28:07 +0200

    admin/member.py: added filter to socis

--- api/admin/member.py
+++ api/admin/member.py
@@ -3,6 +3,33 @@ from django.contrib import admin
 from api.models import Member, Membership
 
 
+class StatusFilter(admin.SimpleListFilter):
+
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Status'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('active', 'active'),
+            ('expired', 'expired'),
+            ('-', '-')
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == '-':
+            value = None
+
+        id_list = [
+            element.pk for element in queryset if element.status == value
+        ]
+        return queryset.filter(pk__in=id_list)
+
+
 class MembershipInLine(admin.TabularInline):
     model = Membership
     extra = 0
@@ -28,6 +55,7 @@ class MemberAdmin(admin.ModelAdmin):
         'type'
     )
     list_display_links = ('number', )
+    list_filter = (StatusFilter, )
     inlines = [MembershipInLine]
 
     def group_display(self, obj):
