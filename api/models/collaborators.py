ed59b4ba20603208899cd71aaf17d3658fe9797c
joni <jonatan@jacoti.com>
2023-11-13 20:43:01 +0100

    Added CollaboratorListSerializer plus order to model

--- api/models/collaborators.py
+++ api/models/collaborators.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+def get_default_order():
+    return Collaborator.objects.count() + 1
+
+
 class Collaborator(models.Model):
     class Meta:
         verbose_name = _('Image')
@@ -11,7 +15,12 @@ class Collaborator(models.Model):
     description = models.TextField(
         verbose_name=_('description'), blank=True, null=True
     )
-    active = models.BooleanField(verbose_name=_('active'), default=True)
+    is_active = models.BooleanField(verbose_name=_('active'), default=True)
+    order = models.IntegerField(
+        verbose_name=_('order'),
+        blank=True,
+        default=get_default_order
+    )
 
     @property
     def url(self):
---
+++ api/serializers/collaborator.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from api.models import Cover
+
+
+class CollaboratorListSerializer(serializers.ModelSerializer):
+    image = serializers.SlugRelatedField(slug_field='url', read_only=True)
+
+    class Meta:
+        model = Cover
+        fields = ['image', 'is_active', 'description']
