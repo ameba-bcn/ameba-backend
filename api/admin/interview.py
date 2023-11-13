3c9abf155012ab11ec313baa0b3825a6db1fef91
joni <jonatan@jacoti.com>
2023-11-13 20:31:22 +0100

    bugfix: question order changes dont crash. Added actions to change from admin list view

--- api/admin/interview.py
+++ api/admin/interview.py
@@ -3,6 +3,7 @@ from api.models import Interview, Answer, Question
 from django.forms.models import BaseInlineFormSet
 from django.utils.html import mark_safe
 from django.utils.translation import gettext as _
+from django.contrib import messages
 
 from django import forms
 from django.forms import ModelForm
@@ -20,6 +21,63 @@ from api.admin.image import get_image_preview
 #         widgets = {
 #             'text': TrumbowygWidget(),
 #         }
+
+def set_position(request, queryset, position):
+    if len(queryset) != 1:
+        messages.add_message(
+            request, messages.WARNING, 'Only one question change at a time'
+        )
+        return
+    queryset.first().set_order(position)
+
+@admin.action(description="Set order to 1")
+def set_first(modeladmin, request, queryset):
+    set_position(request, queryset, 1)
+
+@admin.action(description="Set order to 2")
+def set_second(modeladmin, request, queryset):
+    set_position(request, queryset, 2)
+
+
+@admin.action(description="Set order to 3")
+def set_third(modeladmin, request, queryset):
+    set_position(request, queryset, 3)
+
+
+@admin.action(description="Set order to 4")
+def set_fourth(modeladmin, request, queryset):
+    set_position(request, queryset, 4)
+
+
+@admin.action(description="Set order to 5")
+def set_fifth(modeladmin, request, queryset):
+    set_position(request, queryset, 5)
+
+
+@admin.action(description="Set order to 6")
+def set_sixth(modeladmin, request, queryset):
+    set_position(request, queryset, 6)
+
+
+@admin.action(description="Set order to 7")
+def set_seventh(modeladmin, request, queryset):
+    set_position(request, queryset, 7)
+
+
+@admin.action(description="Set order to 8")
+def set_eighth(modeladmin, request, queryset):
+    set_position(request, queryset, 8)
+
+
+@admin.action(description="Set order to 9")
+def set_ninth(modeladmin, request, queryset):
+    set_position(request, queryset, 9)
+
+
+@admin.action(description="Set order to 10")
+def set_tenth(modeladmin, request, queryset):
+    set_position(request, queryset, 10)
+
 
 
 class ArtistQuestionsInLineFormSet(BaseInlineFormSet):
@@ -79,7 +137,10 @@ admin.site.register(Interview, InterviewAdmin)
 
 class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['question']
-    list_display = ['question']
+    list_display = ['question', 'position']
+    ordering = ['position']
+    actions = [set_first, set_second, set_third, set_fourth, set_fifth,
+               set_sixth, set_seventh, set_eighth, set_ninth, set_tenth]
 
 
 admin.site.register(Question, QuestionAdmin)
