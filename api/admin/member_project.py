0a5191b454cc76e20b0a1843ebe3223ab8695e4b
jon-alamo <jonrivala@gmail.com>
2023-11-30 22:38:11 +0100

    wip: implementing member_projects

---
+++ api/admin/member_project.py
@@ -0,0 +1,63 @@
+from django.contrib import admin
+from django.utils.html import mark_safe
+from modeltranslation.admin import TranslationAdmin
+
+from api.models import Artist, ArtistMediaUrl, ArtistTag
+from api.admin.image import get_image_preview
+
+
+class ArtistTagAdmin(TranslationAdmin):
+    fields = ('name', )
+    list_display = ('name', )
+
+
+class MediaUrlsInLine(admin.StackedInline):
+    model = ArtistMediaUrl
+    verbose_name = 'ArtistMediaUrl'
+    verbose_name_plural = "ArtistMediaUrls"
+    fields = ('embedded', 'created')
+    readonly_fields = ('created', )
+    extra = 0
+
+
+class ArtistImages(admin.StackedInline):
+    model = Artist.images.through
+    verbose_name = 'ArtistImage'
+    verbose_name_plural = 'ArtistImages'
+    fields = ('image', )
+    extra = 0
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['id', 'name', 'is_ameba_dj', 'featured',
+                           'has_interview', 'biography', 'tags', 'created']})
+    ]
+    readonly_fields = ['id', 'bio_preview', 'has_interview']
+    list_display = ['name', 'bio_preview', 'list_preview', 'is_ameba_dj',
+                    'has_interview']
+    inlines = (MediaUrlsInLine, ArtistImages)
+
+    def preview(self, obj):
+        preview = '<div>{images}</div>'
+        images = ''.join(
+            get_image_preview(image, 150) for image in obj.images.all()
+        )
+        return mark_safe(preview.format(images=images))
+
+    preview.short_description = 'Preview'
+    preview.allow_tags = True
+
+    def list_preview(self, obj):
+        preview = '<div>{images}</div>'
+        images = ''.join(
+            get_image_preview(image, 75) for image in obj.images.all()
+        )
+        return mark_safe(preview.format(images=images))
+
+    list_preview.short_description = 'Preview'
+    list_preview.allow_tags = True
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(ArtistTag, ArtistTagAdmin)
