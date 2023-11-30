97f0ff841583a48e88ed073bf873db5a0c79f8a9
jon-alamo <jonrivala@gmail.com>
2023-11-30 19:19:47 +0100

    Añadida acción de cerrar pago en local

--- api/admin/payment.py
+++ api/admin/payment.py
@@ -3,12 +3,24 @@ from django.contrib import admin
 from api.models import Payment
 
 
+@admin.action(description="Close pending payments")
+def close_payment(modeladmin, request, queryset):
+    for payment in queryset:
+        details = dict(invoice=dict(payment.invoice))
+        details['invoice']['status'] = 'paid'
+        details['payment_intent'] = 'payment_intent_id_12345'
+        payment.details = details
+        payment.save()
+        payment.close()
+
+
 class PaymentAdmin(admin.ModelAdmin):
     fields = ['user', 'total', 'status', 'cart_record', 'invoice',
               'timestamp']
     readonly_fields = fields
     list_display = ['user', 'total', 'status', 'timestamp']
     list_filter = ['user']
+    actions = [close_payment]
 
 
 admin.site.register(Payment, PaymentAdmin)
