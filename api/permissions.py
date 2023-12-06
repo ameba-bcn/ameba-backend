83c4f3eb28d8463511e5a9e457ac6c58ccc92eaf
joni <jonatan@jacoti.com>
2023-12-06 19:12:35 +0100

    permissions.py: implemented custom MemberProjectPermissions

--- api/permissions.py
+++ api/permissions.py
@@ -48,3 +48,16 @@ class CartPermission(permissions.IsAuthenticated):
         if not obj.user or request.user == obj.user:
             return True
         return False
+
+
+class MemberProjectPermission(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated:
+            if request.user.member.id == request.data.get('member'):
+                return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if not obj.user or request.user == obj.user:
+            return True
+        return False
