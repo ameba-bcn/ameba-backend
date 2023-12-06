32b6218624e480b667769e8ecbc960cf79f76cb3
joni <jonatan@jacoti.com>
2023-12-06 19:11:45 +0100

    api/serializers/member_project.py: added serializer for MemberProject

--- api/serializers/member_project.py
+++ api/serializers/member_project.py
@@ -8,35 +8,28 @@ class MemberProjectMediaUrlSerializer(serializers.ModelSerializer):
     class Meta:
         model = MemberProjectMediaUrl
         fields = ['embedded']
-
+#
 
 class MemberProjectSerializer(serializers.ModelSerializer):
     media = serializers.SlugRelatedField(
         many=True, slug_field='embedded', read_only=True, source='media_urls'
     )
-    images = serializers.SlugRelatedField(many=True, slug_field='url',
-                                          read_only=True)
     tags = serializers.SlugRelatedField(many=True, slug_field='name',
                                         read_only=True)
 
     class Meta:
         model = MemberProject
-        fields = ['id', 'name', 'description', 'images', 'media', 'tags',
+        fields = ['member', 'name', 'description', 'image', 'media', 'tags',
                   'created', 'public']
         read_only_fields = ['created']
 
 
 
 class MemberProjectListSerializer(serializers.ModelSerializer):
-    images = serializers.SlugRelatedField(many=True, slug_field='url',
-                                          read_only=True)
     tags = serializers.SlugRelatedField(many=True, slug_field='name',
                                         read_only=True)
 
     class Meta:
         model = MemberProject
-        fields = [
-            'id', 'name', 'description_preview', 'images', 'tags',
-            'created', 'public'
-        ]
+        fields = ['id', 'name', 'image', 'tags', 'created', 'public']
         read_only_fields = ['created']
