8ca4861a056631379423218981cccbd298fe53a4
joni <jonatan@jacoti.com>
2023-12-06 19:12:10 +0100

    views/member_project.py: added general CrudViewSet to MemberProject EP

--- api/views/member_project.py
+++ api/views/member_project.py
@@ -1,8 +1,11 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from api.serializers import (
     MemberProjectSerializer, MemberProjectListSerializer
 )
 from api.models import MemberProject
 from api.views.base import BaseCrudViewSet
+from api.permissions import MemberProjectPermission
 
 
 class MemberProjectViewSet(BaseCrudViewSet):
@@ -10,3 +13,10 @@ class MemberProjectViewSet(BaseCrudViewSet):
     detail_serializer = MemberProjectSerializer
     model = MemberProject
     queryset = MemberProject.objects.all().order_by('-created')
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny]
+        elif self.action in ['create', 'update', 'partial_update']:
+            self.permission_classes = [MemberProjectPermission]
+        return super().get_permissions()
