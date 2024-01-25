af0bdb47ada321b3d4ddd016e9d66ae88d0ca25e
jon-alamo <jonrivala@gmail.com>
2024-01-25 12:08:43 +0100

    Removed post_migrate migrations

--- api/apps.py
+++ api/apps.py
@@ -5,6 +5,6 @@ from django.db.models.signals import post_migrate
 class ApiConfig(AppConfig):
     name = 'api'
 
-    def ready(self):
-        from .signals import populate_models
-        post_migrate.connect(populate_models, sender=self)
+    # def ready(self):
+    #     from .signals import populate_models
+    #     post_migrate.connect(populate_models, sender=self)
