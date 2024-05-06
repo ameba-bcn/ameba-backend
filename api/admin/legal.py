69c85432274d37e7db52c3e7685ebd17958d7ff2
jon-alamo <jonrivala@gmail.com>
2024-05-06 10:04:31 +0200

    fix: wrong index attribute in LegalAdmin

--- api/admin/legal.py
+++ api/admin/legal.py
@@ -3,7 +3,7 @@ from api.models import LegalDocument
 
 
 class LegalAdmin(admin.ModelAdmin):
-    ordering = ('index', '-created')
+    ordering = ('-position', '-created')
     fields = ('title', 'description', 'file_name', 'file', 'created', 'updated', 'is_active', 'order')
     readonly_fields = ['file_name', 'size', 'created', 'updated']
     list_display = ('title', 'file_name', 'size', 'created', 'updated', 'is_active')
