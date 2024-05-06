bba455f506d533972cde091a09149dd9eef43b87
jon-alamo <jonrivala@gmail.com>
2024-05-06 10:05:25 +0200

    fix: wrong order attribute in LegalAdmin

--- api/admin/legal.py
+++ api/admin/legal.py
@@ -4,7 +4,7 @@ from api.models import LegalDocument
 
 class LegalAdmin(admin.ModelAdmin):
     ordering = ('-position', '-created')
-    fields = ('title', 'description', 'file_name', 'file', 'created', 'updated', 'is_active', 'order')
+    fields = ('title', 'description', 'file_name', 'file', 'created', 'updated', 'is_active', 'position')
     readonly_fields = ['file_name', 'size', 'created', 'updated']
     list_display = ('title', 'file_name', 'size', 'created', 'updated', 'is_active')
     search_fields = ('title', 'description')
